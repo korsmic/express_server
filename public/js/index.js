1f829a178c2da76183237543fc1be93b92271ad6
Masayoshi Kojima <toriaezu.mk0722@gmail.com>
2017-10-21 11:05:28 +0900

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on("connect", function() {
   console.log("Connected the server");
-
-  socket.emit("createMessage", {
-    from: "jen@exsample.com",
-    text: "Hey this is J"
-  });
 });
 
 socket.on("disconnect", function() {

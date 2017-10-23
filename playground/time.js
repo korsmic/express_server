20bf62a7d3f3faf41db424550ba0c27545faf9d8
Masayoshi Kojima <toriaezu.mk0722@gmail.com>
2017-10-23 16:33:43 +0900

    Scroll to bottom if user is close to bottom

--- playground/time.js
+++ playground/time.js
@@ -1,15 +1,15 @@
 // Jan 1st 1970 00:00:00 am
 const moment = require("moment");
 
-// var date = moment();
-// date.add(100, "Y").subtract(2, "month");
+var date = moment();
 // console.log(date.format("MMM Do, YYYY"));
 
-// 10:35 am
-var someTimeststamp = moment().valueOf();
-console.log(someTimeststamp);
+// // 10:35 am
+// var someTimeststamp = moment().valueOf();
+// console.log(someTimeststamp);
 
-var createdAt = 1234;
-var date = moment(someTimeststamp).format("MMM Do YYYY h:m a");
-console.log(date);
+// var createdAt = 1234;
+// var date = moment().format("h:m a");
+// console.log(date);
 
+console.log(date.format("YYYY年MM月DD日 H時m分"))

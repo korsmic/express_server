88b08ec7d8ef89f02c206a434919ea373e27f845
Masayoshi Kojima <toriaezu.mk0722@gmail.com>
2017-10-24 01:02:38 +0900

    Add Users class and updateUserList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,41 +11,63 @@ function scrollToBottom() {
   var newMessageHeight = newMessage.innerHeight();
   var lastMessageHeight = newMessage.prev().innerHeight();
 
-  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+  if (
+    clientHeight + scrollTop + newMessageHeight + lastMessageHeight >=
+    scrollHeight
+  ) {
     messages.scrollTop(scrollHeight);
   }
-};
+}
 
 socket.on("connect", function() {
-  console.log("Connected the server");
+  var params = jQuery.deparam(window.location.search);
+
+  socket.emit("join", params, function(err) {
+    if (err) {
+      alert(err);
+      // errの場合は"/"へリダイレクト
+      window.location.href = "/";
+    } else {
+      console.log("No error");
+    }
+  });
 });
 
 socket.on("disconnect", function() {
   console.log("Disconnected from server");
 });
 
+socket.on("updateUserList", function(users) {
+  var ol = jQuery("<ol></ol>");
+  users.forEach(function(user) {
+    ol.append(jQuery("<li></li>").text(user));
+  });
+
+  jQuery("#users").html(ol);
+});
+
 socket.on("newMessage", function(message) {
-  var formattedTime = moment().format("h:mm a");
-  var template = jQuery('#message-template').html();
+  var formattedTime = moment(message.createdAt).format("h:mm a");
+  var template = jQuery("#message-template").html();
   var html = Mustache.render(template, {
     text: message.text,
     from: message.from,
     createdAt: formattedTime
   });
 
-  jQuery('#messages').append(html);
+  jQuery("#messages").append(html);
   scrollToBottom();
 });
 
 socket.on("newLocationMessage", function(message) {
-  var formattedTime = moment().format("h:mm a");
-  var template = jQuery('#location-message-template').html();
+  var formattedTime = moment(message.createdAt).format("h:mm a");
+  var template = jQuery("#location-message-template").html();
   var html = Mustache.render(template, {
     from: message.from,
     url: message.url,
     createdAt: formattedTime
   });
-  
+
   jQuery("#messages").append(html);
   scrollToBottom();
 });
@@ -53,16 +75,16 @@ socket.on("newLocationMessage", function(message) {
 jQuery("#message-form").on("submit", function(e) {
   e.preventDefault();
 
-  var messageTextBox = jQuery("[name=message]");
+  var messageTextbox = jQuery("[name=message]");
 
   socket.emit(
     "createMessage",
     {
       from: "User",
-      text: messageTextBox.val()
+      text: messageTextbox.val()
     },
     function() {
-      messageTextBox.val("");
+      messageTextbox.val("");
     }
   );
 });
@@ -73,19 +95,18 @@ locationButton.on("click", function() {
     return alert("Geolocation not supported by your browser");
   }
 
-  locationButton.attr("disabled", "disabled").text("Sending Location...");
+  locationButton.attr("disabled", "disabled").text("Sending location...");
 
   navigator.geolocation.getCurrentPosition(
     function(position) {
-      locationButton.removeAttr("disabled").text("Send Location");
+      locationButton.removeAttr("disabled").text("Send location");
       socket.emit("createLocationMessage", {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
       });
-      console.log("position", position);
     },
     function() {
-      locationButton.removeAttr("disabled").text("Send Location");
+      locationButton.removeAttr("disabled").text("Send location");
       alert("Unable to fetch location.");
     }
   );
--- server/server.js
+++ server/server.js
@@ -4,26 +4,46 @@ const express = require("express");
 const socketIO = require("socket.io");
 
 const { generateMessage, generateLocationMessage } = require("./utils/message");
+const { isRealString } = require("./utils/validation");
+const { Users } = require("./utils/users");
+
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
 
 app.use(express.static(publicPath));
 
 io.on("connection", socket => {
-  console.log("New user connetcted");
+  console.log("New user connected");
 
-  socket.emit(
-    "newMessage",
-    generateMessage("Admin", "Welcome to the chat app")
-  );
+  socket.on("join", (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      return callback("Name and room name are required");
+    }
 
-  socket.broadcast.emit(
-    "newMessage",
-    generateMessage("Admin", "New user joined")
-  );
+    socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id, params.name, params.room);
+
+    io.to(params.room).emit("updateUserList", users.getUserList(params.room));
+
+    socket.emit(
+      "newMessage",
+      generateMessage("Admin", "Welcome to the chat app")
+    );
+
+    socket.broadcast
+      .to(params.room)
+      .emit(
+        "newMessage",
+        generateMessage("Admin", `${params.name} has joined.`)
+      );
+
+    callback();
+  });
 
   // callbackを設定するとフロントのcallbackに値を渡して実行される。
   socket.on("createMessage", (message, callback) => {
@@ -40,10 +60,17 @@ io.on("connection", socket => {
   });
 
   socket.on("disconnect", () => {
-    console.log("User was disconnected");
+    var user = users.removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("updateUserList", users.getUserList(user.room));
+      io
+        .to(user.room)
+        .emit("newMessage", generateMessage("Admin", `${user.name} has left.`));
+    }
   });
 });
 
 server.listen(port, () => {
-  console.log(`server is up on ${port}`);
+  console.log(`Server is up on ${port}`);
 });
